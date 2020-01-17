73d1e63595491a64d376d850a301f4b49ec5d756
subhasreegit <soumyajit.halder@innoraft.com>
2020-01-17 11:33:40 +0530

    Fixed fullname field

--- PHP-Assignments/php-assignment-2.php
+++ PHP-Assignments/php-assignment-2.php
@@ -22,15 +22,28 @@
 <html>
 <head>
 	<title>PHP-ASSIGNMENT-2</title>
+	<script
+        src="https://code.jquery.com/jquery-3.4.1.js"
+        integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+        crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+        $("#firstname,#lastname").change(function(){
+  	    var field1=$("#firstname").val();
+        var field2=$("#lastname").val();
+        $("#fullname").val(field1+" "+field2);
+            });
+        });
+    </script>
 </head>
 <body>
 	<form action="php-assignment-2.php" enctype="multipart/form-data" method="post">
          <label for="first">First name:</label><br>
-  		<input type="text" name="first" required><br>
+  		<input type="text" name="first" id="firstname" required><br>
   		<label for="last">Last name:</label><br>
-  		<input type="text" name="last" required><br><br>
+  		<input type="text" name="last" id="lastname" required><br><br>
   		<label for="full">Full name:</label><br>
-  		<input type="text" name="full" value="<?php echo $fullname?>" disabled><br><br>
+  		<input type="text" name="full" id="fullname" value="<?php echo $fullname?>" disabled><br><br>
   		<input type="file" name="file"><br><br>
   		<input type="submit" value="Submit" name="submitbtn"><br><br>
     </form>

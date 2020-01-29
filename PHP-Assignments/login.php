baf1df9af97c00bc56486f68c7194592fc071097
soumyajithalder <59052643+soumyajithalder@users.noreply.github.com>
2020-01-29 12:27:12 +0530

    Updated login.php

--- PHP-Assignments/login.php
+++ PHP-Assignments/login.php
@@ -1,34 +1,57 @@
 <?php
- session_start();
-$username="admin";
-$password="admin";
+ include"dbconn.php";
 
-if(isset($_POST['login']) && $_POST['username'] == $username && $_POST['password'] == $password){
-    $_SESSION['username']=$username;
+    if(isset($_POST) && !empty($_POST)){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+        
+    $sql= "SELECT * FROM login WHERE username = '".$username."' and password = '".$password."'";
+        
+    $result= $conn->query($sql);
     
-    header('Location: php-assignment-4.php');
+    session_start();
+        
+    if($result->num_rows>0){
+        $user=$result->fetch_assoc();
+        $_SESSION['user']=$user;
+        header('Location: php-assignment-4.php');
+    }
+    else
+    {
+        $_SESSION['user']="0";
+    }
 }
-else{
-    $msg="Username or password not valid";
-}
+
 ?>
+        
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Log In</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="style.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-  <form action="" enctype="multipart/form-data" method="post">
-      <input type="text" name="username" placeholder="Hint: admin" required>
-      <input type="password" name="password" placeholder="Hint: admin" required>
-      <input type="submit" value="Login" name="login">
-     <!--<input type="submit" value="Logout" name="logout">-->
-      <?php
-      if(isset($_POST['login'])){
-          echo $msg;
-      }
-      ?>
-  </form>  
+  <div class="wrapper fadeInDown">
+  <div id="formContent">
+    <form enctype="multipart/form-data" method="post">
+      <input type="text" id="login" class="fadeIn second" name="username" placeholder="Hint:admin" required>
+      <input type="password" id="password" class="fadeIn third" name="password" placeholder="Hint:admin" required>
+      <input type="submit" name="login" class="fadeIn fourth" value="Log In">
+    </form>
+        <?php 
+            if(isset($_SESSION['user'])){ ?>
+             <strong>Sorry !</strong> Email/Password is wrong. SIGN UP or re-enter details.
+            <?php }?>
+    <div class="form-group">
+        Not Registered? <a class="underlineHover" href="signup.php">Sign Up</a>
+    </div>
+
+  </div>
+</div>
 </body>
 </html>

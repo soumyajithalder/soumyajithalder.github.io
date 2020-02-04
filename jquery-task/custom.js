7a4276a5c299ae814eb334aa7f38555866491ca8
subhasreegit <soumyajit.halder@innoraft.com>
2020-02-04 16:14:56 +0530

    Added Jquery

--- jquery-task/custom.js
+++ jquery-task/custom.js
@@ -1,4 +1,4 @@
-function btn1(){
+/*function btn1(){
 document.querySelector(".section1 .height")
         .style.height = '300px';
 }
@@ -82,3 +82,112 @@ function btn9(){
 function btn10(){
     document.querySelector(".section1").scrollIntoView({ behavior: "smooth" })
 }
+*/
+
+
+
+
+$( document ).ready(function() {
+    $("#btn1").click(function()
+    {
+        $(".section1 .height").height(300);
+});
+    
+    $("#btn2").click(function(){
+        var pos=0;
+        var id= setInterval(frame,0);
+        function frame(){
+        if(pos==300){
+            clearInterval(id);
+        }
+        else{
+            pos++;
+        $(".section2 .height").css({marginLeft:pos+"px"});
+        }
+        }
+});
+    
+    
+        var stickyNavTop = $('.full').offset().top;
+		var stickyNav = function(){
+		var scrollTop = $(window).scrollTop();
+		   if (scrollTop > stickyNavTop && scrollTop < 1080) { 
+		       $('.full').addClass('sticky');
+		   } else {
+		       $('.full').removeClass('sticky'); 
+		   }
+		};
+        stickyNav();
+        $(window).scroll(function() {
+		    stickyNav();
+});
+    
+    
+    $("#btn3").click(function()
+    {
+        $(".section5>p").eq(1).wrap("<div></div>");
+        $("#btn3").attr("disabled",true);
+});
+    
+    
+    $("#btn").click(function()
+    {
+        $(".section9 #test3").val("");
+        $("#btn").attr("disabled",true);
+});
+    
+    $("#btn5").click(function()
+    {
+        $(".section6 > p").not(".intro").css({background:"rgba(255, 255, 255, 0)"});
+});
+    
+    
+   $("#btn6").click(function()
+    {
+        $(".section7 > div > ul > li").slice(3,6).css({background:"red"});
+}); 
+    
+    
+  $("#btn7").click(function()
+    {
+        $(".section8 > div > ul > li").not(":first").css({border:"2px solid red"});
+});   
+    
+    
+    $(".section4 .tabcontent > button").click(function()
+    {
+        $(".section4 .tabcontent .content" ).toggleClass( "active" );
+}); 
+    
+    
+    $("#btn10").click(function()
+    {
+         $("html, body").animate({scrollTop: 0}, 1000);
+}); 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});

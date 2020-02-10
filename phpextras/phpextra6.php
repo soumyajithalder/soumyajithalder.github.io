6a23e06966593b17e5bf2a09c607cad695c45af0
subhasreegit <soumyajit.halder@innoraft.com>
2020-02-10 17:18:40 +0530

    Task 6 completed

--- phpextras/phpextra6.php
+++ phpextras/phpextra6.php
@@ -1,46 +1,49 @@
 <?php
 
+$matchWins=array("IND"=>0,"AUS"=>0,"ENG"=>0,"WI"=>0);
+
+$maxRuns=array("match1"=>0,"match2"=>0,"match3"=>0,"match4"=>0,"match5"=>0,"match6"=>0);
+
  $match=array(
 
     "match1" => array(  
     "IND" => array( 
         "IND_1" => "45",  
         "IND_2" => "0", 
-        "IND_3" => "78" 
+        "IND_3" => "45" 
     ),
      "AUS" => array( 
         "AUS_1" => "26",  
         "AUS_2" => "33", 
-        "AUS_3" => "74" 
+        "AUS_3" => "88" 
     )   
 ),
 
     "match2" => array(  
     "IND" => array( 
-        "IND_1" => "19",  
+        "IND_1" => "0",  
         "IND_2" => "1", 
-        "IND_3" => "89" 
+        "IND_3" => "17" 
     ),
      "WI" => array( 
         "WI_1" => "4",  
         "WI_2" => "7", 
-        "WI_3" => "14" 
+        "WI_3" => "78" 
     )   
 ), 
 
     "match3" => array(  
     "IND" => array( 
         "IND_1" => "5",  
-        "IND_2" => "19", 
-        "IND_3" => "35" 
+        "IND_2" => "98", 
+        "IND_3" => "17" 
     ),
      "ENG" => array( 
         "ENG_1" => "54",  
         "ENG_2" => "52", 
-        "ENG_3" => "47" 
+        "ENG_3" => "99" 
     )   
 ), 
-
 
     "match4" => array(  
     "AUS" => array( 
@@ -51,7 +54,7 @@
     "ENG" => array( 
         "ENG_1" => "45",  
         "ENG_2" => "57", 
-        "ENG_3" => "14" 
+        "ENG_3" => "11" 
     )   
 ),
 
@@ -59,7 +62,7 @@
     "match5" => array(  
     "AUS" => array( 
         "AUS_1" => "17",  
-        "AUS_2" => "58", 
+        "AUS_2" => "68", 
         "AUS_3" => "10" 
     ),
      "WI" => array( 
@@ -78,59 +81,64 @@
     ),
      "ENG" => array( 
         "ENG_1" => "6",  
-        "ENG_2" => "8", 
+        "ENG_2" => "88", 
         "ENG_3" => "89" 
     )   
 ));
     
 
 /*-----HIGH SCORER-----*/
+
 $max=array();
 
-echo "<h3>High Scorer</h3>";
+echo "<h3>High Scorers</h3>";
 
-    foreach($match as $data=>$values){
+
+ foreach($match as $data=>$values){
+        echo $data."<br>";
         foreach($values as $teams=>$players){
             //print_r($values);
             $max=array("players"=>array_keys($players,max($players))[0],"score"=>max($players));
+            
             echo "Players: ".$max["players"]." Score ".$max["score"]."<br>";
         }
+     echo "<br>";
+        
+    }
+
+/*-----TOURNAMENT WINNER-----,-----MAXIMUM SCORER-----*/
+
+$score=array();
+
+    foreach($match as $data => $values){
+        foreach($match[$data] as $teams => $values){
+            $sum=0;
+            foreach($match[$data][$teams] as $players => $runs){
+                if(array_key_exists($players,$score)){
+                    $score[$players]+=$runs;
+                }
+                else{
+                    $score+=[$players => $runs];
+                }
+                
+                $sum+=$runs;
+                
+            }
+            if($sum > $maxRuns[$data]){
+                $win=$teams;
+                $maxRuns[$data]=$sum;
+            }
+        }
+        
+        $highestScorer=array_keys($score,max($score));
+        $matchWins[$win]++;
         
     }
 
 
-echo "<h3>Match Winner</h3>";
 
-    $totalRuns=array();
-    $winner=array();
-    foreach($match as $data=>$values){
-        foreach($values as $teams=>$players){
-            //print_r($teams);
-            //$totalRuns=array_sum($players);
-            $totalRuns=array($teams=>array_keys($teams,max($teams))[0],"score"=>array_sum($players));
-            //print_r($totalRuns);
-            $winner = max( array( $totalRuns, $teams["score"] ) );
-            
-        }   
-    }
-    print_r($winner);
-
-
-    echo "<h3>Maximum Scorer</h3>";
-
-    $result = array();
-    foreach ($match as $data => $values) {
-        foreach($values as $teams=>$players){
-            $result=array("players"=>array_keys($players,max($players))[0],"score"=>max($players));
-        }
-    }
-    if($result[0]["score"] < $result[1]["score"]) {
-        echo $result[1]["score"];
-    } else {
-        echo $result[0]["score"];
-    }
-
-
-
-
+echo "<h3>Tournament Winner</h3>";
+    echo array_keys($matchWins,max($matchWins))[0]."<br>";
+echo "<h3>Maximum Scorer</h3>";
+    echo array_keys($score,max($score))[0]."<br>";
 ?>
